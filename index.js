0f67133f0085c17e98883a73abfa37f9c277c870
WillianScotti <138123243+WillianScotti@users.noreply.github.com>
2023-09-27 10:34:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+btn.addEventListener('click',  function(){
+
+var a = parseInt(valorA.value);
+var b = parseInt(valorB.value);
+var op = operacao.value;
+
+if(op == 1){
+ Resultado.innerHTML="Resultado: " + (a+b)
+}
+
+if(op == 2){
+    Resultado.innerHTML="Resultado: " + (a-b)
+    }
+
+if(op == 3){
+        Resultado.innerHTML="Resultado: " + (axb)
+        }
+    
+if(op == 4){
+    Resultado.innerHTML="Resultado: " + (a/b)
+    }
+        
+});
+    
